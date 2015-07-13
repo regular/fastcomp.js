2e323186720a454eb184372099c0acfdc6398a58
Jan Bölsche <regular.gonzales@googlemail.com>
2015-07-13 18:15:43 +0200

    added missing .mem file and test for opt.js

---
+++ test-opt.sh
@@ -0,0 +1,9 @@
+node opt.js -S -O3 -  <<"IR_END"
+define double @test(double %x) {
+entry:
+        %addtmp = fadd double 3.000000e+00, %x
+        %addtmp1 = fadd double %x, 3.000000e+00
+        %multmp = fmul double %addtmp, %addtmp1
+        ret double %multmp
+}
+IR_END
